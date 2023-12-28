d83805e18f78de7d6bfed5a6204298463e3728ce
Shifana nasrin <shifananazrin15@gmail.com>
2023-12-28 23:00:37 +0530

    Deployed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance =axios.create({
-    baseURL:"http://localhost:3000"
+    // baseURL:"http://localhost:3000"
+    baseURL:"https://glossary-management.onrender.com"
+
 });
 
 export default instance;
